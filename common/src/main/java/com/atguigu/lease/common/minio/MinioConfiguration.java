5444f295040f61a6cbd914d0584d3478674074b7
3227708047 <17262150506@163.com>
2025-06-04 11:04:03 +0800

    上传文件接口实现

---
+++ common/src/main/java/com/atguigu/lease/common/minio/MinioConfiguration.java
@@ -0,0 +1,22 @@
+package com.atguigu.lease.common.minio;
+
+import io.minio.MinioClient;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ConfigurationPropertiesScan("com.atguigu.lease.common.minio")
+public class MinioConfiguration {
+
+    @Autowired
+    private MinioProperties properties;
+
+
+    @Bean
+    public MinioClient client() {
+       return MinioClient.builder().endpoint(properties.getEndpoint()).credentials(properties.getAccessKey(), properties.getSecretKey()).build();
+    }
+}
